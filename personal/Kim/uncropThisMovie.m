209dc01d583b11aa2857a329b237556bd9642157
kimerein <kimerein@gmail.com>
2017-02-20 15:25:54 -0500

    Added check and handling so that no pixel is constant throughout movie -- required for CNMF

--- personal/Kim/uncropThisMovie.m
+++ personal/Kim/uncropThisMovie.m
@@ -1,5 +1,7 @@
 function movStruct=uncropThisMovie(movStruct,cropHere)
 
+persistent fakeData
+
 % Uncrop, fill with nans
 mov=nan(length(cropHere.rows),length(cropHere.columns),size(movStruct.slice.channel.mov,3));
 rowInds=find(cropHere.rows==0);
@@ -13,4 +15,22 @@ for i=1:length(columnInds)
     ci=columnInds(i);
     mov(:,ci,:)=temp(:,i,:);
 end
+
+if isempty(fakeData)
+   fakeData=randi(2,1,size(mov,3)); 
+else
+   if length(fakeData)~=size(mov,3)
+       fakeData=randi(2,1,size(mov,3)); 
+   end
+end
+
+% Check that no pixels are always zero
+% This check is required for movie to work with CNMF Ca2+ source detection
+movsum=reshape(sum(mov,3),size(mov,1),size(mov,2));
+[rowind,columnind]=find(movsum==0 | all(isnan(movsum),3));
+mov(isnan(mov))=0;
+for i=1:length(rowind)
+    mov(rowind(i),columnind(i),:)=mov(rowind(i),columnind(i),:)+reshape(fakeData,1,1,length(fakeData));
+end
+
 movStruct.slice.channel.mov=mov;
